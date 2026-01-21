8bead39e41abf1e1db3106907f14fbeb8fe21616
abhishek panwar <abhishekpawar725@gmail.com>
2026-01-21 16:50:11 +0530

    folder structure done

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,11 @@
+import Header from "../ui/header";
+import Footer from "../ui/footer";
+export default function MainLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
+}
---
+++ app/ui/branded-solution/footer.tsx
@@ -0,0 +1,3 @@
+export default function Footer(){
+    return <h1>branded solution footer</h1>
+}
