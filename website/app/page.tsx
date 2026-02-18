795352e7cdcc24dd21b98e3aa1f89bb03add2461
abhishek panwar <abhishekpawar725@gmail.com>
2026-02-18 18:18:46 +0530

    child category show

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -2,17 +2,21 @@ import { getCategory } from "@/lib/category";
 import Link from "next/link";
 export default async function Home() {
   const categories = await getCategory();
-  console.log(categories);
+  const rootCategories = categories.filter((cat: any) => cat.parentId === null);
   return (
     <>
       <ul>
-        {categories
-          .filter((cat: any) => cat.parentId !== null)
-          .map((cat: any) => (
-            <div key={cat._id}>
-              <Link href={`category/${cat.slug}`}>{cat.name}</Link>
-            </div>
-          ))}
+        {rootCategories.map((root: any) => (
+          <li key={root._id}>
+            {categories
+              .filter((cat: any) => cat.parentId !== null && cat.parentId?._id === root._id)
+              .map((child: any) => (
+                <div key={child._id}>
+                  <Link href={`category/${child.slug}`}>{child.name}</Link>
+                </div>
+              ))}
+          </li>
+        ))}
       </ul>
     </>
   );
