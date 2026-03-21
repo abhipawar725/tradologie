c8b435b83ef787d8d9f2d2c888bfcf6c9cfb50ff
abhishek panwar <abhishekpawar725@gmail.com>
2026-03-21 18:03:30 +0530

    product slug logic complete

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -25,6 +25,24 @@ export const FetchById = async (req, res) => {
   }
 };
 
+export const FetchBySlug = async (req, res) => {
+  try {
+    const {slug} = req.params
+    if(!slug) return res.status(400).json({message: 'Slug is required'})
+
+    const product = await Product.findOne({
+      slug: slug.toLowerCase()
+    })
+    .select("-__v")
+    .lean();
+    
+    if(!product) return res.status(404).json({message: 'Product not found'})
+    return res.status(200).json({product})  
+  } catch (error) {
+     res.status(500).json({message: error.message})    
+  }
+}
+
 export const Create = async (req, res) => {
   try {
     const { name, category, slug, shortDescription, isActive, showInHome } = req.body;
--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -1,11 +1,12 @@
 import {Router} from "express"
-import { Create, Fetch, Update, Delete, FetchById } from "../controllers/product.controller.js";
+import { Create, Fetch, Update, Delete, FetchById, FetchBySlug } from "../controllers/product.controller.js";
 import upload from "../middlewares/upload.middleware.js";
 
 const productRouter = Router()
 
 productRouter.get("/", Fetch)
 productRouter.get("/:id", FetchById)
+productRouter.get("/slug/:slug", FetchBySlug)
 productRouter.post("/", upload.single('image'), Create)
 productRouter.put("/:id",upload.single('image'), Update)
 productRouter.delete("/:id", Delete)
